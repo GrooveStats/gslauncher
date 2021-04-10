522bb4334e10aeb933dbded2484c1b43b3a810da
Martin Natano <natano@natano.net>
2021-04-10 12:58:07 +0200

    Add logo to windows executables

--- build/build-win.sh
+++ build/build-win.sh
@@ -8,6 +8,7 @@ export GOOS=windows
 export CC=x86_64-w64-mingw32-gcc
 export GOARCH=amd64
 
+x86_64-w64-mingw32-windres -o ../cmd/gslauncher/logo.syso logo.rc
 go build -o gslauncher-windows-amd64.exe ../cmd/gslauncher
 go build -tags debug -o gslauncher-windows-amd64-debug.exe ../cmd/gslauncher
 
@@ -15,5 +16,9 @@ go build -tags debug -o gslauncher-windows-amd64-debug.exe ../cmd/gslauncher
 export CC=i686-w64-mingw32-gcc
 export GOARCH=386
 
+i686-w64-mingw32-windres -o ../cmd/gslauncher/logo.syso logo.rc
 go build -o gslauncher-windows-i386.exe ../cmd/gslauncher
 go build -tags debug -o gslauncher-windows-i386-debug.exe ../cmd/gslauncher
+
+
+rm ../cmd/gslauncher/logo.syso
