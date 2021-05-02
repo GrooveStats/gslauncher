96e7bdf14873da2a5f6561e29d51d2765b3f0dbf
Martin Natano <natano@natano.net>
2021-05-02 11:00:23 +0200

    Check for portable.ini on windows to improve autodetection

--- internal/settings/detect_windows.go
+++ internal/settings/detect_windows.go
@@ -5,52 +5,37 @@ import (
 	"path/filepath"
 )
 
-const (
-	sm50ExePath = "C:\\Games\\StepMania 5\\Program\\StepMania.exe"
-	sm51ExePath = "C:\\Games\\StepMania 5.1\\Program\\StepMania.exe"
-	sm53ExePath = "C:\\Games\\StepMania 5.3 Outfox\\Program\\StepMania.exe"
-)
+var dirnames = []string{
+	"StepMania 5.1",
+	"StepMania 5.3 Outfox",
+	"StepMania 5",
+}
 
 func detectSM() (string, string) {
-	var sm50, sm51, sm53 bool
+	for _, dirname := range dirnames {
+		installDir := filepath.Join("C:\\Games", dirname)
+		smExePath := filepath.Join(installDir, "Program\\StepMania.exe")
 
-	_, err := os.Stat(sm50ExePath)
-	if err == nil {
-		sm50 = true
-	}
-
-	_, err = os.Stat(sm51ExePath)
-	if err == nil {
-		sm51 = true
-	}
-
-	_, err = os.Stat(sm53ExePath)
-	if err == nil {
-		sm53 = true
-	}
-
-	var smExePath, smDataDir string
-
-	switch {
-	case sm51:
-		smExePath = sm51ExePath
-
-		configDir, err := os.UserConfigDir()
-		if err == nil {
-			smDataDir = filepath.Join(configDir, "StepMania 5.1")
+		_, err := os.Stat(smExePath)
+		if err != nil {
+			continue
 		}
-	case sm53:
-		// 5.3 is a portable installation by default
-		smExePath = sm53ExePath
-		smDataDir = "C:\\Games\\StepMania 5.3 Outfox"
-	case sm50:
-		smExePath = sm50ExePath
 
-		configDir, err := os.UserConfigDir()
+		var smDataDir string
+
+		// portable installation?
+		_, err = os.Stat(filepath.Join(installDir, "portable.ini"))
 		if err == nil {
-			smDataDir = filepath.Join(configDir, "StepMania 5")
+			smDataDir = installDir
+		} else {
+			configDir, err := os.UserConfigDir()
+			if err == nil {
+				smDataDir = filepath.Join(configDir, dirname)
+			}
 		}
+
+		return smExePath, smDataDir
 	}
 
-	return smExePath, smDataDir
+	return "", ""
 }
