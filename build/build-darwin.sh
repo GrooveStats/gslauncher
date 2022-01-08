1466e6bd187f49de15e57588b07c4339a0fcba74
Martin Natano <martin.natano@wegfinder.at>
2022-01-08 19:01:38 +0100

    Build universal binary for macos

--- build/build-darwin.sh
+++ build/build-darwin.sh
@@ -4,20 +4,36 @@ set -eux
 cd "$(dirname $0)/.."
 SRCDIR="$(pwd)"
 
+export CGO_ENABLED=1
+export GOOS=darwin
+
+export GOARCH=amd64
+go build -o dist/gslauncher-darwin-amd64 ./cmd/gslauncher
+go build -tags debug -o dist/gslauncher-darwin-amd64-debug ./cmd/gslauncher
+
+export GOARCH=arm64
+go build -o dist/gslauncher-darwin-arm64 ./cmd/gslauncher
+go build -tags debug -o dist/gslauncher-darwin-arm64-debug ./cmd/gslauncher
+
+unset GOARCH
+
+lipo -create -output dist/gslauncher-darwin dist/gslauncher-darwin-{amd64,arm64}
+lipo -create -output dist/gslauncher-darwin-debug dist/gslauncher-darwin-{amd64,arm64}-debug
+
 go install fyne.io/fyne/v2/cmd/fyne@latest
 
 TMPDIR="$(mktemp -d)"
 cd $TMPDIR
-fyne package -release -icon "${SRCDIR}/build/logo.png" -sourceDir "${SRCDIR}/cmd/gslauncher" -name 'GrooveStats Launcher'
+fyne package -release -executable "${SRCDIR}/dist/gslauncher-darwin" -icon "${SRCDIR}/build/logo.png" -name 'GrooveStats Launcher'
 ln -s /Applications Applications
 cd -
-hdiutil create dist/gslauncher-macos-amd64.dmg -fs HFS+ -volname 'GrooveStats Launcher' -format UDZO -srcfolder $TMPDIR
+hdiutil create dist/gslauncher-macos.dmg -fs HFS+ -volname 'GrooveStats Launcher' -format UDZO -srcfolder $TMPDIR
 rm -r $TMPDIR
 
 TMPDIR="$(mktemp -d)"
 cd $TMPDIR
-fyne package -tags debug -icon "${SRCDIR}/build/logo.png" -sourceDir "${SRCDIR}/cmd/gslauncher" -name 'GrooveStats Launcher (debug)'
+fyne package -executable "${SRCDIR}/dist/gslauncher-darwin-debug" -icon "${SRCDIR}/build/logo.png" -name 'GrooveStats Launcher (debug)'
 ln -s /Applications Applications
 cd -
-hdiutil create dist/gslauncher-macos-amd64-debug.dmg -fs HFS+ -volname 'GrooveStats Launcher (debug)' -format UDZO -srcfolder $TMPDIR
+hdiutil create dist/gslauncher-macos-debug.dmg -fs HFS+ -volname 'GrooveStats Launcher (debug)' -format UDZO -srcfolder $TMPDIR
 rm -r $TMPDIR
